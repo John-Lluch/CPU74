528fa2eccac8d09474a37ea6be52130919001a59
joanlluch <joan.lluch@icloud.com>
2019-10-12 18:13:36 +0200

    9.0

--- Simulator/Simulator/Machine.swift
+++ Simulator/Simulator/Machine.swift
@@ -234,41 +234,43 @@ class Machine
     0b010_1110  :  (add_kq,    .me_end),
     0b010_1111  :  (jmp_k,     .me_wait),
     
-    // Types I2, R2, R1, J
-    0b011_0000  :  (set_cr,    .me_end),
-    0b011_0001  :  (sel_crrr,  .me_end),
-    0b011_0010  :  (br_ck,     .me_end),
-    0b011_0011  :  (nop,       .me_end),
-    0b011_0100  :  (nop,       .me_end),
-    0b011_0101  :  (nop,       .me_end),
-    0b011_0110  :  (nop,       .me_end),
-    0b011_0111  :  (nop,       .me_end),
-    0b011_1000  :  (nop,       .me_end),
-    0b011_1001  :  (nop,       .me_end),
-    0b011_1010  :  (mov_kr,    .me_end),
-    0b011_1011  :  (cmp_rk,    .me_end),
-    0b011_1100  :  (add_kr,    .me_end),
-    0b011_1101  :  (sub_kr,    .me_end),
-    0b011_1110  :  (and_kr,    .me_end),
-    0b011_1111  :  (movw_ar,   .me_load_w),
+        // Types P, I1, I2
+    0b011_0000  :  (movsb_ar,  .me_load_sb),
+    0b011_0001  :  (movw_ra,   .me_store_w),
+    0b011_0010  :  (movb_ra,   .me_store_b),
+    0b011_0011  :  (lea_qr,    .me_end),
+    0b011_0100  :  (movw_qr,   .me_load_w),
+    0b011_0101  :  (movsb_qr,  .me_load_sb),
+    0b011_0110  :  (movw_rq,   .me_store_w),
+    0b011_0111  :  (movb_rq,   .me_store_b),
+    0b011_1000  :  (lea_mr,    .me_end),
+    0b011_1001  :  (movw_mr,   .me_load_w),
+    0b011_1010  :  (movzb_mr,  .me_load_zb),
+    0b011_1011  :  (movsb_mr,  .me_load_sb),
+    0b011_1100  :  (movw_rm,   .me_store_w),
+    0b011_1101  :  (movb_rm,   .me_store_b),
+    0b011_1110  :  (call_k,    .me_call_k1),
+    0b011_1111  :  (_pfix,     .me_end),
     
-    // Types P, I1, I2
-    0b100_0000  :  (movsb_ar,  .me_load_sb),
-    0b100_0001  :  (movw_ra,   .me_store_w),
-    0b100_0010  :  (movb_ra,   .me_store_b),
-    0b100_0011  :  (lea_qr,    .me_end),
-    0b100_0100  :  (movw_qr,   .me_load_w),
-    0b100_0101  :  (movsb_qr,  .me_load_sb),
-    0b100_0110  :  (movw_rq,   .me_store_w),
-    0b100_0111  :  (movb_rq,   .me_store_b),
-    0b100_1000  :  (lea_mr,    .me_end),
-    0b100_1001  :  (movw_mr,   .me_load_w),
-    0b100_1010  :  (movzb_mr,  .me_load_zb),
-    0b100_1011  :  (movsb_mr,  .me_load_sb),
-    0b100_1100  :  (movw_rm,   .me_store_w),
-    0b100_1101  :  (movb_rm,   .me_store_b),
-    0b100_1110  :  (call_k,    .me_call_k1),
-    0b100_1111  :  (_pfix,     .me_end),
+    // Types I2, R2, R1, J (miscelaneous)
+    0b100_0000  :  (set_cr,    .me_end),
+    0b100_0001  :  (sel_crrr,  .me_end),
+    0b100_0010  :  (br_ck,     .me_end),
+    0b100_0011  :  (nop,       .me_end),
+    0b100_0100  :  (nop,       .me_end),
+    0b100_0101  :  (nop,       .me_end),
+    0b100_0110  :  (nop,       .me_end),
+    0b100_0111  :  (nop,       .me_end),
+    0b100_1000  :  (nop,       .me_end),
+    0b100_1001  :  (nop,       .me_end),
+    0b100_1010  :  (mov_kr,    .me_end),
+    0b100_1011  :  (cmp_rk,    .me_end),
+    0b100_1100  :  (add_kr,    .me_end),
+    0b100_1101  :  (sub_kr,    .me_end),
+    0b100_1110  :  (and_kr,    .me_end),
+    0b100_1111  :  (movw_ar,   .me_load_w),
+    
+
   ]
   
   //-------------------------------------------------------------------------------------------
@@ -285,111 +287,239 @@ class Machine
     reg.sp = mem.size  // FIX ME: Should this be automatically done by the machine setup code ??
   }
   
+  
+  
+//    func decode()
+//  {
+//
+//    // Decoding bits (level 1)
+//
+//    //let a0 = !ir[15] && !ir[14] && !ir[13]  // 000_xx_xx_xx_x
+//    let a1 = !(!ir[15] && !ir[14] &&  ir[13])  // 001_xx_xx_xx_x
+//    let a2 = !(!ir[15] &&  ir[14] && !ir[13])  // 010_xx_xx_xx_x
+//    let a3 = !(!ir[15] &&  ir[14] &&  ir[13])  // 011_xx_xx_xx_x
+//    let a4 = !( ir[15] && !ir[14] && !ir[13])  // 100_xx_xx_xx_x
+//    let a5 = !( ir[15] && !ir[14] &&  ir[13])  // 101_xx_xx_xx_x
+//    let a6 = !( ir[15] &&  ir[14] && !ir[13])  // 110_xx_xx_xx_x
+//    //let a7 =  ir[15] &&  ir[14] &&  ir[13]  // 111_xx_xx_xx_x
+//
+//    let b3 =  !(ir[12] &&  ir[11])             // xxx_11_xx_xx_x
+//
+//    //let c3 =  ir[10]                        // xxx_xx_1_xxx_x
+//
+//    let d0 =  !(!ir[8] && !ir[7])              // xxx_xx_xx_00_x
+//    let d1 =  !(!ir[8] &&  ir[7])              // xxx_xx_xx_01_x
+//    let d3 =  !(ir[8] &&  ir[7])              // xxx_xx_xx_11_x
+//
+////    let i0 =  !ir[12]                       // xxx_0x_xx_xx_x
+////    let i1 =   ir[12]                       // xxx_1x_xx_xx_x
+//
+//    // Decoding bits (level 2)
+//
+//    let n0 =  !(!ir[15] && !ir[14] && !ir[13] && !ir[12])    // 0000_xxxxxx
+//    let n1 =  !(!ir[15] && !ir[14] && !ir[13] &&  ir[12])    // 0001_xxxxxx
+//    let n4 =  !(!ir[15] &&  ir[14] && !ir[13] && !ir[12])    // 0100_xxxxxx
+//    let n5 =  !(!ir[15] &&  ir[14] && !ir[13] &&  ir[12])    // 0101_xxxxxx
+//    let n6 =  !(!ir[15] &&  ir[14] &&  ir[13] && !ir[12])    // 0110_xxxxxx
+//    let n7 =  !(!ir[15] &&  ir[14] &&  ir[13] &&  ir[12])    // 0111_xxxxxx
+//    let n14 = !( ir[15] &&  ir[14] &&  ir[13] && !ir[12])    // 1110_xxxxxx
+//    let n15 = !( ir[15] &&  ir[14] &&  ir[13] &&  ir[12])    // 1111_xxxxxx
+//
+//
+////    let n0 =  a0 && i0    // 0000_xxxxxx
+////    let n1 =  a0 && i1    // 0001_xxxxxx
+////    let n4 =  a2 && i0    // 0100_xxxxxx
+////    let n5 =  a2 && i1    // 0101_xxxxxx
+////    let n6 =  a3 && i0    // 0110_xxxxxx
+////    let n7 =  a3 && i1    // 0111_xxxxxx
+////    let n14 = a7 && i0    // 1110_xxxxxx
+////    let n15 = a7 && i1    // 1111_xxxxxx
+//
+//    // Decode register and condition code operands
+//
+//    rd = ir[2,0]
+//    rs = ir[5,3]
+//    rn = ir[8,6]
+//    cc = ir[11,9]
+//
+//    // Decode embeeded immediate fields
+//
+//    if !n15 {
+//      imm = ir[10,0] }       // Type P, zero extended 11 bit
+//
+//    if !(a6 && n14) {
+//      imm = ir[10,6] }       // Type I1, zero extended 5 bit
+//
+//    if !n5 {
+//      imm = ir.sext(10,3) }  // Type I2, sign extended 8 bit
+//
+//    if !(a3 && a4 && a5) {
+//      imm = ir[10,3] }       // Type I2, zero extended 8 bit
+//
+//    if !( n4 && (a1 || b3 || !ir[10])) {
+//      imm = ir.sext(8,0) }   // Type J, sign extended 9 bit
+//
+//    imm = imm | (rk.value<<5)
+//
+//    // Instruction decode
+//
+//    var oh:UInt16 = 0     // xxx
+//    var ol:UInt16 = 0     // yyyy
+//
+//    if ir[15]                        { oh = 4 }   // Types P, I1, I2  with 1oooo
+//    if (!n0 && !d3) || !n1 || !a2 || !a3  { oh = 3 }   // Types J, R1, R2  with 0000ccc11x, 0001ccc, 010_0ccc,
+//                                                  // Types I2         with 010_1o, 011_0o, 011_1o
+//
+//    if !a1                            { oh = 2 }   // Types J, R1      with 001oooo
+//    if !(n0 || d1)                      { oh = 1 }   // type R2          with 0000ooo01o
+//    if !(n0 || d0)                      { oh = 0 }   // Type R2          with 0000ooo00o
+//
+//    if ir[15]                   { ol = ir[14,11] }                 // Types P, I1, I2  with 1oooo
+//    if !(n5 && n6 && n7)           { ol = ir[14,11] }                 // Types I2    with 0101o, 0110o, 0111o
+//    if !n4                       { ol = 2 }                         // Type J      with 0100ccc
+//    if !n1                       { ol = 1 }                         // Type R1     with 0001ccc
+//    if !(n0 || d3)                 { ol = 0 }                         // Type R2     with 0000ccc11x
+//    if !a1                       { ol = ir[12,9] }                  // Type J, R1  with 001oooo
+//    if !n0 && !(d0 && d1)         { ol = (ir[6,6]<<3) | ir[11,9] }   // Type R2     with 0000ooo00o, 0000ooo01o
+//
+//    // Get the instruction opcode
+//    let op =  br_inhibit ? 0b111_0000 :
+//              control.1 != .me_end ? mir : (oh<<4) | ol
+//    // Log
+//    if out.logEnabled { logDecode() }
+//
+////    if (prg.pc == 356 )
+////    {
+////      let stop = 1
+////    }
+//
+//    // Decode the instruction (get the instruction control bits)
+//    if let f = instrP0[op] { control = f }  // Decoder Pattern
+//    else { out.exitWithError( "Unrecognized instruction opcode" ) }
+//
+//    // These can only be set to true by the exec code, so we clear it first
+//    prg.parsel = false
+//    br_inhibit = false
+//    pc_inhibit = false
+//
+//  }
+//
+  
   func decode()
   {
-  
+
     // Decoding bits (level 1)
-    
-    let a0 = !ir[15] && !ir[14] && !ir[13]  // 000_xx_xx_xx_x
+
+    //let a0 = !ir[15] && !ir[14] && !ir[13]  // 000_xx_xx_xx_x
     let a1 = !ir[15] && !ir[14] &&  ir[13]  // 001_xx_xx_xx_x
     let a2 = !ir[15] &&  ir[14] && !ir[13]  // 010_xx_xx_xx_x
     let a3 = !ir[15] &&  ir[14] &&  ir[13]  // 011_xx_xx_xx_x
     let a4 =  ir[15] && !ir[14] && !ir[13]  // 100_xx_xx_xx_x
     let a5 =  ir[15] && !ir[14] &&  ir[13]  // 101_xx_xx_xx_x
     let a6 =  ir[15] &&  ir[14] && !ir[13]  // 110_xx_xx_xx_x
-    let a7 =  ir[15] &&  ir[14] &&  ir[13]  // 111_xx_xx_xx_x
-    
+    //let a7 =  ir[15] &&  ir[14] &&  ir[13]  // 111_xx_xx_xx_x
+
     let b3 =  ir[12] &&  ir[11]             // xxx_11_xx_xx_x
-    
-    let c3 =  ir[10]                        // xxx_xx_1_xxx_x
-    
+
+    //let c3 =  ir[10]                        // xxx_xx_1_xxx_x
+
     let d0 =  !ir[8] && !ir[7]              // xxx_xx_xx_00_x
     let d1 =  !ir[8] &&  ir[7]              // xxx_xx_xx_01_x
     let d3 =   ir[8] &&  ir[7]              // xxx_xx_xx_11_x
-    
-    let i0 =  !ir[12]                       // xxx_0x_xx_xx_x
-    let i1 =   ir[12]                       // xxx_1x_xx_xx_x
-    
+
+//    let i0 =  !ir[12]                       // xxx_0x_xx_xx_x
+//    let i1 =   ir[12]                       // xxx_1x_xx_xx_x
+
     // Decoding bits (level 2)
-    
-    let n0 =  a0 && i0    // 0000_xxxxxx
-    let n1 =  a0 && i1    // 0001_xxxxxx
-    let n4 =  a2 && i0    // 0100_xxxxxx
-    let n5 =  a2 && i1    // 0101_xxxxxx
-    let n6 =  a3 && i0    // 0110_xxxxxx
-    let n7 =  a3 && i1    // 0111_xxxxxx
-    let n14 = a7 && i0    // 1110_xxxxxx
-    let n15 = a7 && i1    // 1111_xxxxxx
-  
+
+    let n0 =  !ir[15] && !ir[14] && !ir[13] && !ir[12]    // 0000_xxxxxx
+    let n1 =  !ir[15] && !ir[14] && !ir[13] &&  ir[12]    // 0001_xxxxxx
+    let n4 =  !ir[15] &&  ir[14] && !ir[13] && !ir[12]    // 0100_xxxxxx
+    let n5 =  !ir[15] &&  ir[14] && !ir[13] &&  ir[12]    // 0101_xxxxxx
+    let n6 =  !ir[15] &&  ir[14] &&  ir[13] && !ir[12]    // 0110_xxxxxx
+    let n7 =  !ir[15] &&  ir[14] &&  ir[13] &&  ir[12]    // 0111_xxxxxx
+    let n14 =  ir[15] &&  ir[14] &&  ir[13] && !ir[12]    // 1110_xxxxxx
+    let n15 =  ir[15] &&  ir[14] &&  ir[13] &&  ir[12]    // 1111_xxxxxx
+
+
+//    let n0 =  a0 && i0    // 0000_xxxxxx
+//    let n1 =  a0 && i1    // 0001_xxxxxx
+//    let n4 =  a2 && i0    // 0100_xxxxxx
+//    let n5 =  a2 && i1    // 0101_xxxxxx
+//    let n6 =  a3 && i0    // 0110_xxxxxx
+//    let n7 =  a3 && i1    // 0111_xxxxxx
+//    let n14 = a7 && i0    // 1110_xxxxxx
+//    let n15 = a7 && i1    // 1111_xxxxxx
+
     // Decode register and condition code operands
-    
+
     rd = ir[2,0]
     rs = ir[5,3]
     rn = ir[8,6]
     cc = ir[11,9]
-    
+
     // Decode embeeded immediate fields
-    
+
     if n15 {
       imm = ir[10,0] }       // Type P, zero extended 11 bit
-    
+
     if a6 || n14 {
       imm = ir[10,6] }       // Type I1, zero extended 5 bit
-    
+
     if n5 {
       imm = ir.sext(10,3) }  // Type I2, sign extended 8 bit
-    
+
     if a3 || a4 || a5 {
       imm = ir[10,3] }       // Type I2, zero extended 8 bit
-    
-    if n4 || (a1 && b3 && c3) {
+
+    if n4 || (a1 && b3 && ir[10]) {
       imm = ir.sext(8,0) }   // Type J, sign extended 9 bit
-    
+
     imm = imm | (rk.value<<5)
-    
+
     // Instruction decode
-    
+
     var oh:UInt16 = 0     // xxx
     var ol:UInt16 = 0     // yyyy
-    
-    if ir[15]                        { oh = 4 }   // Types P, I1, I2  with 1oooo
-    if (n0 && d3) || n1 || a2 || a3  { oh = 3 }   // Types J, R1, R2  with 0100ccc, 0001ccc, 0000ccc11x,
-                                                  // Types I2         with 0101o, 0110o, 0111o
-    
+
+    //if (n0 && d3) || n1 || a2 || a3  { oh = 4 }   // Types J, R1, R2  with 0000ccc11x, 0001ccc, 010_0ccc,
+                                                  // Types I2         with 010_1o, 011_0o, 011_1o
+    oh = 4;
+    if ir[15]                        { oh = 3 }   // Types P, I1, I2  with 1oooo
     if a1                            { oh = 2 }   // Types J, R1      with 001oooo
     if n0 && d1                      { oh = 1 }   // type R2          with 0000ooo01o
     if n0 && d0                      { oh = 0 }   // Type R2          with 0000ooo00o
-  
+
     if ir[15]                   { ol = ir[14,11] }                 // Types P, I1, I2  with 1oooo
     if n5 || n6 || n7           { ol = ir[14,11] }                 // Types I2    with 0101o, 0110o, 0111o
     if n4                       { ol = 2 }                         // Type J      with 0100ccc
+    if a1                       { ol = ir[12,9] }                  // Type J, R1  with 001oooo
     if n1                       { ol = 1 }                         // Type R1     with 0001ccc
     if n0 && d3                 { ol = 0 }                         // Type R2     with 0000ccc11x
-    if a1                       { ol = ir[12,9] }                  // Type J, R1  with 001oooo
     if n0 && (d0 || d1)         { ol = (ir[6,6]<<3) | ir[11,9] }   // Type R2     with 0000ooo00o, 0000ooo01o
-    
+
     // Get the instruction opcode
     let op =  br_inhibit ? 0b111_0000 :
               control.1 != .me_end ? mir : (oh<<4) | ol
     // Log
     if out.logEnabled { logDecode() }
-    
+
 //    if (prg.pc == 356 )
 //    {
 //      let stop = 1
 //    }
-  
+
     // Decode the instruction (get the instruction control bits)
     if let f = instrP0[op] { control = f }  // Decoder Pattern
     else { out.exitWithError( "Unrecognized instruction opcode" ) }
-    
+
     // These can only be set to true by the exec code, so we clear it first
     prg.parsel = false
     br_inhibit = false
     pc_inhibit = false
-    
+
   }
- 
+//
   //-------------------------------------------------------------------------------------------
   // This represents the execution of control signals
   func process() -> Bool
